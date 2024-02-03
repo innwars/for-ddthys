bbde3f34d52c1434b10104ccb48632346fad24b4
Jaeyun <158548835+innwars@users.noreply.github.com>
2024-02-03 13:10:36 -0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.wrapper').addEventListener('click', function() {
-    const flap = document.querySelector('.env::before');
-    const letter = document.querySelector('.lettre');
-    // Toggle open class or directly apply styles here for animation
-    flap.style.transform = 'rotateX(-180deg)'; // This might need to be adjusted based on your setup
-    letter.style.transform = 'translateY(-100%)';
-});
